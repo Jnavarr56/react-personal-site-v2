e2705240ed19e192a8810414da7471607ee992c2
Jorge Navarro <jnavarr56@gmail.com>
2025-11-26 02:42:56 -0500

    fix

--- src/routes.js
+++ src/routes.js
@@ -16,22 +16,22 @@ const routes = [
 		path: 'about-me',
 		fadeInContent: true
 	},
-	{
-		title: { en: 'Skills', es: 'Habilidades' },
-		showTitle: true,
-		showParticles: false,
-		component: <Skills />,
-		path: 'skills'
-	},
-	{
-		title: { en: 'Projects', es: 'Proyectos' },
-		showTitle: true,
-		showParticles: false,
-		component: <Projects />,
-		path: 'projects',
-		ignorePadding: true,
-		fadeInContent: true
-	},
+	// {
+	// 	title: { en: 'Skills', es: 'Habilidades' },
+	// 	showTitle: true,
+	// 	showParticles: false,
+	// 	component: <Skills />,
+	// 	path: 'skills'
+	// },
+	// {
+	// 	title: { en: 'Projects', es: 'Proyectos' },
+	// 	showTitle: true,
+	// 	showParticles: false,
+	// 	component: <Projects />,
+	// 	path: 'projects',
+	// 	ignorePadding: true,
+	// 	fadeInContent: true
+	// },
 	{
 		title: { en: 'Contact', es: 'Información de Contacto' },
 		showTitle: true,
